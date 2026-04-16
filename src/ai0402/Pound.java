6fbf12c7ed732eb6e2f9e5310f57a8f023f90ddc
2601110279-stack <2601110279@gspace.kopo.ac.kr>
2026-04-16 10:13:28 +0900

    java

--- src/ai0402/Pound.java
+++ src/ai0402/Pound.java
@@ -1,4 +1,19 @@
 package ai0402;
 
+import java.util.Scanner;
+
 public class Pound {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("파운드(lb)를 입력하시오 :  ");
+        int num1 = s.nextInt();
+
+        System.out.printf("%d 파운드(lb)는 %.2f킬로그램(kg)입니다.\n", num1, num1 * 0.45359237);
+
+        System.out.print("킬로그램(kg)를 입력하시오 :  ");
+        int num2 = s.nextInt();
+
+        System.out.printf("%d 킬로그램(kg)는 %.2f파운드(lb)입니다.\n", num2, num2 * 2.20462262);
+    }
 }
--- src/ai0402/true_or_false.java
+++ src/ai0402/true_or_false.java
@@ -1,4 +1,24 @@
 package ai0402;
 
+import java.util.Scanner;
+
 public class true_or_false {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("운전면허 시점 점수 : ");
+        int num1 = s.nextInt();
+
+        String result = "";
+
+        if(num1 >= 70)
+            result = "합격";
+        else
+            result = "불합격";
+
+        System.out.println("====운전면허 시점 결과====");
+
+        System.out.printf("%s입니다.", result);
+        s.close();
+    }
 }
